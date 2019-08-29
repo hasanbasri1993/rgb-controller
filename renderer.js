f5e7904886e1908d1582b448943cfb5149b432e2
Hasan Basri <hasanbasri1493@gmail.com>
2019-08-29 08:47:54 +0700

    first commit

---
+++ renderer.js
@@ -0,0 +1,55 @@
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+var port = '';
+
+SerialPort.list((err, ports) => {
+
+  
+  var elm = document.getElementById('portSelect'),
+    df = document.createDocumentFragment();
+  ports.forEach(function (port) {
+    var option = document.createElement('option');
+    option.value = port.comName;
+    option.appendChild(document.createTextNode(port.comName));
+    df.appendChild(option);
+  });
+  elm.appendChild(df);
+  
+
+})
+
+
+function connect(a) {
+  port = new SerialPort(a, { baudRate: 115200 });
+  const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+  // Read the port data
+  port.on("open", () => {
+    console.log('serial port open');
+    M.toast({ html: 'serial port open: ' + a, classes: 'rounded' })
+    $('main').fadeIn(2000);
+
+  });
+  parser.on('data', data => {
+    console.log('got word from arduino:', data);
+    document.getElementById("log").innerHTML = data;
+    M.toast({ html: data, classes: 'rounded' })
+  });
+}
+
+/* function exec(a) {
+  port.write('m ' + a + '\n', (err) => {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+} */
+
+//function set(a) {
+function exec(a) {
+  port.write(a + '\n', (err) => {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+  });
+}
