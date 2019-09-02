859745e7593dbae9afc9acd27eaba777144ae5e0
Hasan Basri <hasanbasri1493@gmail.com>
2019-09-02 23:27:53 +0700

    pilih port

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 const electron = require('electron')
-
-    // Module to control application life.
-const app = electron.app
-    // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
-
 const path = require('path')
-const url = require('url')
+const url = require('url');
+
+const { app, BrowserWindow, Menu } = electron;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -15,31 +11,48 @@ let mainWindow
 function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 1200,
-        height: 700,
+        width: 1000,
+        height: 800,
         webPreferences: {
-            nodeIntegration: true,
-            preload: path.join(__dirname, 'preload.js')
+            nodeIntegration: true
         }
     })
 
     // and load the index.html of the app.
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'web.html'),
+        pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }))
+
+    var menu = Menu.buildFromTemplate([
+        {
+            label: 'Menu',
+            submenu: [
+                { label: 'About' },
+                {
+                    label: 'Exit', click() {
+                        app.quit();
+                    },
+                    accelerator: 'CmdOrCtrl+Q'
+                }
+            ]
+        }
+    ])
+   // Menu.setApplicationMenu(menu);
 
     // Open the DevTools.
     //mainWindow.webContents.openDevTools()
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
+    mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         mainWindow = null
     })
+
+
 }
 
 // This method will be called when Electron has finished
@@ -48,13 +61,13 @@ function createWindow() {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     app.quit()
 })
 
-app.on('activate', function() {
+app.on('activate', function () {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
--- renderer.js
+++ renderer.js
@@ -1,51 +1,56 @@
-const SerialPort = require('serialport');
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline');
 var port = '';
 
-SerialPort.list((err, ports) => {
+function listPort() {
+  var e = document.getElementById('portSelect');
 
-  
-  var elm = document.getElementById('portSelect'),
-    df = document.createDocumentFragment();
-  ports.forEach(function (port) {
-    var option = document.createElement('option');
-    option.value = port.comName;
-    option.appendChild(document.createTextNode(port.comName));
-    df.appendChild(option);
-  });
-  elm.appendChild(df);
-  
+  SerialPort.list((err, ports) => {
+    var elm = document.getElementById('portSelect'),
+      df = document.createDocumentFragment();
 
-})
+    ports.forEach(function (port) {
+      var option = document.createElement('option');
+      option.value = port.comName;
+      option.appendChild(document.createTextNode(port.comName));
+      df.appendChild(option);
+    });
 
+    elm.appendChild(df);
+  })
+}
 
 function connect(a) {
   port = new SerialPort(a, { baudRate: 115200 });
   const parser = port.pipe(new Readline({ delimiter: '\n' }));
-
   // Read the port data
   port.on("open", () => {
-    console.log('serial port open');
     M.toast({ html: 'serial port open: ' + a, classes: 'rounded' })
-    $('main').fadeIn(2000);
-
   });
+
   parser.on('data', data => {
-    console.log('got word from arduino:', data);
     document.getElementById("log").innerHTML = data;
-    M.toast({ html: data, classes: 'rounded' })
+    console.log(data);
+    if (!data.includes("getsegments")) {
+      M.toast({ html: data, classes: 'rounded' })
+    }
+
+    if (data.includes("getsegments")) {
+      setTimeout(function () { load() }, 2000);
+    }
+
+    if (data.includes("Starting Up")) {
+      $('main').fadeIn(2000);
+      json = '{"command":"getsegments"}';
+      exec(JSON.stringify(json));
+    }
   });
 }
 
-/* function exec(a) {
-  port.write('m ' + a + '\n', (err) => {
-    if (err) {
-      return console.log('Error on write: ', err.message);
-    }
-  });
-} */
-
-//function set(a) {
 function exec(a) {
   port.write(a + '\n', (err) => {
     if (err) {
