1f95c4c6e6b12e60c2caac9bc8620d23113d9c41
Hasan Basri <hasanbasri1493@gmail.com>
2019-09-10 22:55:23 +0700

    autoload
    delete web.html
    detect arduino port
    set autoload web startup

--- renderer.js
+++ renderer.js
@@ -3,8 +3,6 @@ const Readline = require('@serialport/parser-readline');
 var port = '';
 
 SerialPort.list((err, ports) => {
-
-  
   var elm = document.getElementById('portSelect'),
     df = document.createDocumentFragment();
   ports.forEach(function (port) {
@@ -14,8 +12,6 @@ SerialPort.list((err, ports) => {
     df.appendChild(option);
   });
   elm.appendChild(df);
-  
-
 })
 
 
@@ -27,13 +23,20 @@ function connect(a) {
   port.on("open", () => {
     console.log('serial port open');
     M.toast({ html: 'serial port open: ' + a, classes: 'rounded' })
-    $('main').fadeIn(2000);
+
+
 
   });
   parser.on('data', data => {
     console.log('got word from arduino:', data);
     document.getElementById("log").innerHTML = data;
     M.toast({ html: data, classes: 'rounded' })
+
+    //https://www.w3schools.com/jsref/jsref_includes.asp
+    if (data.includes("Starting Up")) {
+      $('main').fadeIn(2000);
+      load();
+    }
   });
 }
 
